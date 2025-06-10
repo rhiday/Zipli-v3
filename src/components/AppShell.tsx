82c3e93d3b825386808a705dc7079d26844afbfc
Shahriar Rhiday <shahriar.rhiday@gmail.com>
2025-06-10 15:39:24 +0300

    fix: remove BottomNav globally from AppShell (no bottom nav on any page)

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -36,7 +36,7 @@ const AppShell: React.FC<AppShellProps> = ({ children }) => {
             <main className="flex-grow px-0 pb-[76px] md:pb-0">
               {children}
             </main>
-            <BottomNav />
+            {/* <BottomNav /> Removed globally as per request */}
           </div>
         </div>
       )}
